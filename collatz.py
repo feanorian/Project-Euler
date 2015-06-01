2555dfe9c3ef192703ce10810f4d38dfc0c3e2c7
Craig L Brooks <Craig.Brooks95@myhunter.cuny.edu>
2015-06-01 06:20:06 -0400

    This module produces a Collatz string for any given integer input

---
+++ collatz.py
@@ -0,0 +1,14 @@
+import math
+
+def collatz(x):
+    seq = [x]
+    if x < 1:
+        return []
+    while x > 1:
+        if x % 2 == 0:
+            x = x / 2
+        else:
+            x = 3 * x + 1
+        seq.append(x)
+        seq.sort()
+    return seq
